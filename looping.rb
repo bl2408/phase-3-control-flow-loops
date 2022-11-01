9c73891ce4ed9c082ac1a67b64ac03adbfaf1808
Brian Lambert <brianlambert2408@gmail.com>
2022-11-01 16:15:39 +1100

    Completed tasks

--- looping.rb
+++ looping.rb
@@ -1,5 +1,13 @@
 def happy_new_year
   # your code here
+  countdown = 10
+
+  while countdown > 0
+    puts countdown
+    countdown -= 1
+  end
+
+  puts "Happy New Year!"
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
@@ -17,8 +25,16 @@ end
 
 def fizzbuzz_printer
   # your code here
+  (1..100).each do |num|
+    puts fizzbuzz(num)
+  end
 end
 
 def reverse_string(str)
   # your code here
+  newStr = ""
+  str.length.times do |i|
+    newStr = str[i] + newStr
+  end
+  newStr
 end
